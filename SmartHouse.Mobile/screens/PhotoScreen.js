ef4b73cf8f43dd8bc4c62224c81c3e68bb767969
Djenan Lizde <djenan.lizde@edu.fit.ba>
2021-05-30 19:09:29 +0200

    Added photos to photos screen

--- SmartHouse.Mobile/screens/PhotoScreen.js
+++ SmartHouse.Mobile/screens/PhotoScreen.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import Arduino from '../assets/Arduino.jpg';
+import ESP32 from '../assets/ESP32.jpg';
 
 const PhotoScreen = () => {
     return (
       <View style={styles.container}>
-        <Text>Photo Screen</Text>
+        <Text style={styles.text}>Arduino uno - PIR sensor</Text>
+        <Image
+          style={styles.logo}
+          source={Arduino}
+        />
+        <Text style={styles.text}>ESP32 Wroom - DHT11, Servo</Text>
+        <Image
+          style={styles.logo}
+          source={ESP32}
+        />
       </View>
     );
 };
@@ -17,4 +28,13 @@ const styles = StyleSheet.create({
     alignItems: 'center', 
     justifyContent: 'center'
   },
+  logo: {
+    width: 200,
+    height: 300,
+    marginBottom: 30
+  },
+  text:{
+    marginBottom:5,
+    fontSize:20
+  }
 });
